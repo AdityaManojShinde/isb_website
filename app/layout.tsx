968d881005f42ef62e4e828e34083e09cf457282
AdityaManojShinde <aditya.manoj.shinde@gmail.com>
2025-03-28 16:00:51 +0530

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/sonner';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { Rocket } from "lucide-react";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ISB - Indian Startup Business',
-  description: 'Empowering Indian Startups with Innovative Solutions',
-  keywords: 'Indian startups, business consulting, web development, mobile development, UI/UX design',
+  title: "ISB : Indian Startup Business",
+  description: "Empowering Indian Startups with Innovative Solutions",
+  keywords:
+    "Indian startups, business consulting, web development, mobile development, UI/UX design",
 };
 
 export default function RootLayout({
